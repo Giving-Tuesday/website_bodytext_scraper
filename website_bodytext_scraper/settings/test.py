565c4751499dc556d0dbf56533e162778b910f9f
Britt Witham <brittany.witham@gmail.com>
2024-10-22 13:31:42 -0400

    feat: adds batched outputs

--- website_bodytext_scraper/settings/test.py
+++ website_bodytext_scraper/settings/test.py
@@ -6,12 +6,15 @@ SCRAPER_ENV='test'
 
 # Inputs & outputs
 INPUT_PATH = 'website_bodytext_scraper/data/oct_11_run/bodytext_inputs_test.csv'
-OUTPUT_PATH = 's3://non-profit-web-scraping-dev/tests/TEST.csv'
+OUTPUT_PATH = 's3://non-profit-web-scraping-dev/tests/oct_22_run/%(batch_time)s'
+
+# Batching outputs
+# NOTE: filename MUST contain `%(batch_time)s` or `%(batch_id)d` to export correctly
+FEED_EXPORT_BATCH_ITEM_COUNT = 100
 
 # Log level
 LOG_LEVEL = 'DEBUG'
-# LOG_FORMAT = '%(levelname)s: %(message)s'
-# LOG_FILE = 'exports/logs/test_logs.txt'
+LOG_FILE = 'exports/logs/test_logs.txt'
 
 # AWS CREDENTIALS
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
