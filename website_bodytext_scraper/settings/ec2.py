063c830bf91b9fb9bdc54d4ec7ab8345678dc4ea
Sana Farooqui <45830329+phoolish-philomath@users.noreply.github.com>
2024-12-03 15:29:42 -0500

    Update website_bodytext_scraper/settings/ec2.py

--- website_bodytext_scraper/settings/ec2.py
+++ website_bodytext_scraper/settings/ec2.py
@@ -16,4 +16,4 @@ LOG_FILE = 'exports/logs/logs.txt'
 EXTENSIONS = {
    'scrapy.extensions.closespider.CloseSpider': 500,
 }
-CLOSESPIDER_TIMEOUT = 21300 # 5 hours 55 mins (5 mins before cron reset)
+CLOSESPIDER_TIMEOUT = 110 * 60 # 1 hour 50 mins (10 mins before cron reset)
